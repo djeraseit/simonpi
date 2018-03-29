fad45b3e4f89f128dac0de63c07900bafdbaddca
M0Rf30 <morf3089@gmail.com>
2018-03-29 21:40:26 +0200

    some improvements on docker side

--- docker/resources/docker-entrypoint.sh
+++ docker/resources/docker-entrypoint.sh
@@ -7,8 +7,25 @@ apk update && apk add --no-cache bash binutils coreutils dnsmasq dosfstools e2fs
 	qemu-system-arm qemu-system-aarch64 sudo tar unzip util-linux wget
 
 echo "===> Networking settings ..."
-mkdir -p /dev/net
-mknod /dev/net/tun c 10 200
+# Create the kvm node (required --privileged)
+if [ ! -e /dev/kvm ]; then
+   set +e
+   mknod /dev/kvm c 10 $(grep '\<kvm\>' /proc/misc | cut -f 1 -d' ')   
+   set -e
+fi
+
+# If we have a BRIDGE_IF set, add it to /etc/qemu/bridge.conf
+   mkdir -p /etc/qemu
+   echo "allow rasp-br0" >> /etc/qemu/bridge.conf
+
+   # Make sure we have the tun device node
+   if [ ! -e /dev/net/tun ]; then
+      set +e
+      mkdir -p /dev/net
+      mknod /dev/net/tun c 10 $(grep '\<tun\>' /proc/misc | cut -f 1 -d' ')
+      set -e
+   fi
+fi
 
 ## install_from_git
 install_from_git() {
